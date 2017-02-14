8ba1749feef171f90ddb98461a3a186a0318dfd0
Erica Lee <erica.lee@students.olin.edu>
2017-02-14 16:21:51 -0500

    ADD: Index and TermCounter from day00

--- search-engine/day01/src/Index.java
+++ search-engine/day01/src/Index.java
@@ -1,3 +1,75 @@
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 public class Index {
-    // TODO Replace this with your Index.java class
+
+	// Index: map of words to URL and their counts
+	private Map<String, Set<TermCounter>> index = new HashMap<String, Set<TermCounter>>();
+
+	public void add(String term, TermCounter tc) {
+        Set<TermCounter> set = get(term);
+        if (set == null){
+            set = new HashSet<>();
+            index.put(term,set);
+        }
+
+        set.add(tc);
+
+		// if we're seeing a term for the first time, make a new Set
+		// otherwise we can add the term to an existing Set
+	}
+
+	public Set<TermCounter> get(String term) {
+	    return index.get(term);
+	}
+
+	public void indexPage(String url, Elements paragraphs) {
+		// make a TermCounter and count the terms in the paragraphs
+        TermCounter tc = new TermCounter(url);
+        tc.processElements(paragraphs);
+
+		// for each term in the TermCounter, add the TermCounter to the index
+		for(String term : tc.keySet()){
+		    add(term, tc);
+        }
+	}
+
+	public void printIndex() {
+		// loop through the search terms
+		for (String term: keySet()) {
+			System.out.println(term);
+
+			// for each term, print the pages where it appears
+			Set<TermCounter> tcs = get(term);
+			for (TermCounter tc: tcs) {
+				Integer count = tc.get(term);
+				System.out.println("    " + tc.getLabel() + " " + count);
+			}
+		}
+	}
+
+	public Set<String> keySet() {
+		return index.keySet();
+	}
+
+	public static void main(String[] args) throws IOException {
+
+		WikiFetcher wf = new WikiFetcher();
+		Index indexer = new Index();
+
+		String url = "https://en.wikipedia.org/wiki/Java_(programming_language)";
+		Elements paragraphs = wf.fetchWikipedia(url);
+		indexer.indexPage(url, paragraphs);
+
+		url = "https://en.wikipedia.org/wiki/Programming_language";
+		paragraphs = wf.fetchWikipedia(url);
+		indexer.indexPage(url, paragraphs);
+
+		indexer.printIndex();
+	}
 }
