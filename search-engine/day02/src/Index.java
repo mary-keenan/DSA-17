d46d51b71ab3102156a683d33f46cbf3feda93e3
Erica Lee <erica.lee@students.olin.edu>
2017-02-17 15:41:27 -0500

    COPY: files from day 1

--- search-engine/day02/src/Index.java
+++ search-engine/day02/src/Index.java
@@ -1,10 +1,92 @@
+import org.jsoup.select.Elements;
 import redis.clients.jedis.Jedis;
+import redis.clients.jedis.Transaction;
 
-import java.util.Map;
+import java.io.IOException;
+import java.util.*;
 
 public class Index {
-    // TODO Replace this with your Index.java class
 
-    Index(Jedis j){}
+	// Index: map of words to URL and their counts
+	private Map<String, Set<TermCounter>> index = new HashMap<>();
+	private Jedis jedis = JedisMaker.make();
+	private StopWords stopWords = new StopWords();
 
+    public Index() throws IOException {
+    }
+
+    public void add(String term, TermCounter tc) {
+        Set<TermCounter> set = get(term);
+        if (set == null){
+            set = new HashSet<>();
+            index.put(term,set);
+        }
+
+        set.add(tc);
+
+		// if we're seeing a term for the first time, make a new Set
+		// otherwise we can add the term to an existing Set
+	}
+
+	public Set<TermCounter> get(String term) {
+	    return index.get(term);
+	}
+
+	public void indexPage(String url, Elements paragraphs) throws IOException {
+		// make a TermCounter and count the terms in the paragraphs
+        TermCounter tc = new TermCounter(url);
+        tc.processElements(paragraphs);
+
+        Transaction t = jedis.multi();
+
+        String hashname = "TermCounter: " + url;
+        t.del(hashname);        //delete if already indexed
+
+        Set<String> badWords = stopWords.getStopWords();
+
+        // for each term in the TermCounter, add the TermCounter to the index
+		for(String term : tc.keySet()){
+		    if (!badWords.contains(term)) {
+                add(term, tc);
+                t.hset(hashname, term, tc.get(term).toString());
+                String urlSetKey = "urlSet: " + term;
+                t.sadd(urlSetKey, url);
+            }
+        }
+        List<Object> res = t.exec();
+//        System.out.println(res.toString());
+	}
+
+	public void printIndex() {
+		// loop through the search terms
+		for (String term: keySet()) {
+//			System.out.println(term);
+
+			// for each term, print the pages where it appears
+			Set<TermCounter> tcs = get(term);
+			for (TermCounter tc: tcs) {
+				Integer count = tc.get(term);
+//				System.out.println("    " + tc.getLabel() + " " + count);
+			}
+		}
+	}
+
+	public Set<String> keySet() {
+		return index.keySet();
+	}
+
+	public static void main(String[] args) throws IOException {
+		WikiFetcher wf = new WikiFetcher();
+		Index indexer = new Index();
+
+		String url = "https://en.wikipedia.org/wiki/Java_(programming_language)";
+		Elements paragraphs = wf.fetchWikipedia(url);
+		indexer.indexPage(url, paragraphs);
+
+		url = "https://en.wikipedia.org/wiki/Programming_language";
+		paragraphs = wf.fetchWikipedia(url);
+		indexer.indexPage(url, paragraphs);
+
+		indexer.printIndex();
+	}
 }
--- search-engine/day02/src/TermCounter.java
+++ search-engine/day02/src/TermCounter.java
@@ -1,3 +1,96 @@
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+import org.jsoup.nodes.Node;
+import org.jsoup.nodes.TextNode;
+import org.jsoup.select.Elements;
+
 public class TermCounter {
-    // TODO Replace this with your TermCounter.java class
+
+	private Map<String, Integer> map;
+	private String label;
+
+	public TermCounter(String label) {
+		this.label = label;
+		this.map = new HashMap<String, Integer>();
+	}
+
+	public String getLabel() {
+		return label;
+	}
+
+	public int size() {
+		int count = 0;
+		for (String term: map.keySet()) {
+			count += get(term);
+		}
+		return count;
+	}
+
+	public void processElements(Elements paragraphs) {
+		for (Node node: paragraphs) {
+			processTree(node);
+		}
+	}
+
+	public void processTree(Node root) {
+		// NOTE: we could use select to find the TextNodes, but since
+		// we already have a tree iterator, let's use it.
+		for (Node node: new WikiNodeIterable(root)) {
+			if (node instanceof TextNode) {
+				processText(((TextNode) node).text());
+			}
+		}
+	}
+
+	public void processText(String text) {
+		// replace punctuation with spaces, convert to lower case, and split on whitespace
+		String[] array = text.replaceAll("\\pP", " ").toLowerCase().split("\\s+");
+
+		// increment the count for each term
+		for (String term: array) {
+			incrementTermCount(term);
+		}
+	}
+
+	public void incrementTermCount(String term) {
+		put(term, get(term) + 1);
+	}
+
+	public void put(String term, int count) {
+		map.put(term, count);
+	}
+
+	public Integer get(String term) {
+		Integer count = map.get(term);
+		if (count != null) {
+			return count;
+		}
+		return 0;
+	}
+
+	public Set<String> keySet() {
+		return map.keySet();
+	}
+
+	public void printCounts() {
+		for (String key: keySet()) {
+			Integer count = get(key);
+			System.out.println(key + ", " + count);
+		}
+		System.out.println("Total of all counts = " + size());
+	}
+
+	public static void main(String[] args) throws IOException {
+		String url = "https://en.wikipedia.org/wiki/Java_(programming_language)";
+
+		WikiFetcher wf = new WikiFetcher();
+		Elements paragraphs = wf.fetchWikipedia(url);
+
+		TermCounter counter = new TermCounter(url.toString());
+		counter.processElements(paragraphs);
+		counter.printCounts();
+	}
 }
