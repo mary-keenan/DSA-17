bafafffae0054bc05e93199dd17f9c3194a79246
mary-keenan <mary.keenan@students.olin.edu>
2017-04-10 15:52:53 -0400

    all tests passing

--- greedy/src/FirstFailingVersion.java
+++ greedy/src/FirstFailingVersion.java
@@ -1,7 +1,34 @@
 public class FirstFailingVersion {
 
     public static long firstBadVersion(long n, IsFailingVersion isBadVersion) {
-        // TODO
-        return -1;
+        long firstBad = mergeSearch(0, n, isBadVersion);
+
+        return firstBad;
+    }
+
+    public static long mergeSearch(long lowerBound, long upperBound, IsFailingVersion isBadVersion) {
+        //base case
+        if (upperBound - lowerBound <= 1) { //we're donesies
+            if (isBadVersion.isFailingVersion(lowerBound)) {
+                return lowerBound;
+            } else {
+                return upperBound;
+            }
+        }
+
+        long firstBad;
+        long middle = (upperBound + lowerBound)/2; //rounds down
+        Boolean isBad = isBadVersion.isFailingVersion(middle);
+
+        if (isBad) { //if it's bad, go left
+            if (!isBadVersion.isFailingVersion(middle - 1)) { //if the middle number is the first bad number, return it
+                return middle;
+            } else {
+                firstBad = mergeSearch(lowerBound, middle - 1, isBadVersion);
+            }
+        } else { //if it's good, go right
+            firstBad = mergeSearch(middle + 1, upperBound, isBadVersion);
+        }
+        return firstBad;
     }
 }
--- greedy/src/MixingMilk.java
+++ greedy/src/MixingMilk.java
@@ -1,7 +1,44 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 
 public class MixingMilk {
-    public static int solve(int M, int N, int[] units, int[] price) {
-        // TODO
-        return 0;
+    public static int solve(int M, int N, int[] units, int[] price) { //TODO: units and prices lists are mixed up???
+        int currMilkSum = 0;
+        int currMilkCost = 0;
+        HashMap<Integer, Integer> quantityCostMap = new HashMap<>(); //quantityCostMap[cost] = quantity
+        ArrayList costList = new ArrayList(); //we'll sort this --> get lowest cost --> access quantity from map
+
+        //SORT FARMER'S QUANTITIES BY COST -- STORE IN HASHMAP
+        for (int n = 0; n < N; n++) {
+            if (quantityCostMap.get(units[n]) == null) { //if this cost isn't already in the map, just add it normally
+                quantityCostMap.put(units[n], price[n]);
+                costList.add(units[n]);
+            } else { //otherwise update current value -- keeping track of specific farmers doesn't matter, just overall cost
+                quantityCostMap.put(units[n], quantityCostMap.get(units[n]) + price[n]);
+            }
+        }
+        Collections.sort(costList); //sorts in ascending order
+
+        //RUN THROUGH COST LIST/QUANTITY MAP UNTIL WE HAVE ENOUGH MILK
+        while (currMilkSum < M) {
+            if (costList.size() > 0) {
+                int currMinCost = (int) costList.get(0);
+                int costQuantity = quantityCostMap.get(currMinCost);
+                if (M >= currMilkSum + costQuantity) { //if this won't take us over the limit, continue
+                    currMilkSum += costQuantity;
+                    currMilkCost += costQuantity * currMinCost;
+                } else {
+                    int diff = M - currMilkSum;
+                    currMilkSum += diff;
+                    currMilkCost += diff * currMinCost;
+                }
+                costList.remove(0);
+            }
+            else { //might as well end this loop
+                currMilkSum = M;
+            }
+        }
+        return currMilkCost;
     }
 }
