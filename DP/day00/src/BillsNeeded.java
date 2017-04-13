2bd166d932ac7e6b7cd7f47970fc93d67bbe9496
mary-keenan <mary.keenan@students.olin.edu>
2017-04-12 20:01:53 -0400

    passing bills/stocks tests -- bills take 2.5 minutes to run though

--- DP/day00/src/BillsNeeded.java
+++ DP/day00/src/BillsNeeded.java
@@ -4,10 +4,10 @@ import java.util.HashMap;
 public class BillsNeeded {
 
     public int billsNeeded(int N, int[] billDenominations) {
-        return pickBill(N, 0, 0, billDenominations, new HashMap());
+        return pickBill(N, 0, 0, billDenominations, new HashMap(), new HashMap<>());
     }
 
-    private int pickBill(int N, int currSum, int numCurrBills, int[] billDenomination, HashMap sumMap) {
+    private int pickBill(int N, int currSum, int numCurrBills, int[] billDenomination, HashMap<Integer, Integer> sumMap, HashMap<Integer, Integer> billNumMap) {
 
         //base cases 1/2 -- we hit the number or passed it
         if (currSum == N) {
@@ -20,16 +20,13 @@ public class BillsNeeded {
         int pathNumBills;
         //iterate through bills
         for (int bill: billDenomination) {
-            if (sumMap.get(currSum + bill) != null) { //we've already tried this path
-                ArrayList key = new ArrayList();
-                key.add(currSum + bill, numCurrBills);
-                pathNumBills = (int) sumMap.get(key);
-            } else { //first time down this path
-                pathNumBills = pickBill(N, currSum + bill, numCurrBills + 1, billDenomination, sumMap);
-                ArrayList key = new ArrayList();
-                key.add(currSum + bill);
-                key.add(numCurrBills);
-                sumMap.put(key, pathNumBills); //put it in the map
+            int key = currSum + bill;
+            if (sumMap.get(key) != null && sumMap.get(key) < numCurrBills) { //we've already tried this path and it's faster
+                pathNumBills = billNumMap.get(key);
+            } else { //first time down this path or we're off to a faster start
+                pathNumBills = pickBill(N, currSum + bill, numCurrBills + 1, billDenomination, sumMap, billNumMap);
+                sumMap.put(key, numCurrBills); //put it in the map
+                billNumMap.put(key, pathNumBills);
             }
             if (pathNumBills < minNumBills) {
                 minNumBills = pathNumBills;
