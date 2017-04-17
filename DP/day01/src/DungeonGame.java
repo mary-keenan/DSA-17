655bb39451d3481af53a63d1cbfa51e0c4962a05
mary-keenan <mary.keenan@students.olin.edu>
2017-04-17 16:24:55 -0400

    going to revamp minEdit, use bottom-up

--- DP/day01/src/DungeonGame.java
+++ DP/day01/src/DungeonGame.java
@@ -1,7 +1,45 @@
 public class DungeonGame {
 
     public static int minInitialHealth(int[][] map) {
-        // TODO: Your code here
-        return 0;
+        int initHealth = tabulateHealth(map);
+        return initHealth;
+    }
+
+    private static int tabulateHealth(int[][] map) {
+        //get map boundaries
+        int width = map.length;
+        int height = map[0].length;
+
+        //create N x M matrix with maximum health balances so far
+        int[][] dipMap = new int[width][height];
+        for (int x = 0; x < width; x++) { //start in top right corner
+            for (int y = 0; y < height; y++) {
+                int currBoxVal = map[x][y];
+                int boxBelowVal = Integer.MAX_VALUE;
+                int boxLeftVal = Integer.MAX_VALUE;
+                if (y > 0) boxBelowVal = dipMap[x][y - 1];
+                if (x > 0) boxBelowVal = dipMap[x - 1][y];
+                if (boxBelowVal == Integer.MAX_VALUE && boxLeftVal == Integer.MAX_VALUE) boxBelowVal = 0; //if we're in the bottom right corner, cost is 0
+                int lowerCost = Math.min(boxBelowVal, boxLeftVal);
+                dipMap[x][y] = currBoxVal + lowerCost;
+            }
+        }
+
+        //go through matrix and replace current health balances with worst dip seen so far
+        for (int x = 0; x < width; x++) { //start in top right corner
+            for (int y = 0; y < height; y++) {
+                int currBoxVal = map[x][y];
+                int boxBelowVal = Integer.MIN_VALUE;
+                int boxLeftVal = Integer.MIN_VALUE;
+                if (y > 0) boxBelowVal = dipMap[x][y - 1];
+                if (x > 0) boxBelowVal = dipMap[x - 1][y];
+                if (boxBelowVal == Integer.MIN_VALUE && boxLeftVal == Integer.MIN_VALUE) boxBelowVal = 0; //if we're in the bottom right corner, worst health so far is 0
+                int highestDip = Math.max(boxBelowVal, boxLeftVal);
+                if (currBoxVal < highestDip) dipMap[x][y] = currBoxVal;
+                else dipMap[x][y] = highestDip;
+            }
+        }
+
+        return Math.abs(dipMap[0][0]) + 1;
     }
 }
--- DP/day01/src/EditDistance.java
+++ DP/day01/src/EditDistance.java
@@ -5,7 +5,7 @@ public class EditDistance {
 
     public static int minEditDist(String a, String b) {
         //create memo
-        int[][] memo = new int[b.length() + 1][b.length() + 1]; //[numTrans][aInd]
+        int[][] memo = new int[b.length() + 1][a.length() + 1]; //[bInd][aInd]
         for (int i=0; i <= b.length(); i++) {
             Arrays.fill(memo[i], -1);
         }
@@ -16,27 +16,30 @@ public class EditDistance {
 
     private static int makeChoice(String a, String b, int[][] memo, int aInd, int bInd, int numTrans) {
         //check if memoized already -- no reason to go further if it is
-        if (memo[numTrans][aInd] != -1) {
-            return memo[numTrans][aInd];
-        }
+//        if (memo[bInd][aInd] != -1) {
+//            return memo[bInd][aInd];
+//        }
+
         //check base cases -- reached the bottom or right edge of our sub-problem matrix
-        if (aInd == a.length() || numTrans == b.length()) {
-            if (bInd >= b.length() - 1 && aInd >= a.length() - 1) return Integer.MAX_VALUE; //we never finished transforming the string
-            else return numTrans;
+        if (a.length() != 0 && aInd == a.length() || bInd == b.length()) {
+            if (bInd == b.length() && aInd == a.length())
+                return numTrans; //we finished transforming the string
+            else return Integer.MAX_VALUE;
         }
         //if current letter in A matches current letter in B, do not make any transformation
-        if (a.charAt(aInd) == b.charAt(bInd)) {
-            memo[numTrans][aInd] = makeChoice(a, b, memo, aInd + 1, bInd + 1, numTrans);
-            return memo[numTrans][aInd];
+        if (aInd < a.length() && a.charAt(aInd) == b.charAt(bInd)) {
+            memo[bInd][aInd] = makeChoice(a, b, memo, aInd + 1, bInd + 1, numTrans);
+        } else if (aInd >= a.length()) { //if index has gone past a's length, you can ONLY INSERT
+            memo[bInd][aInd] = makeChoice(a, b, memo, aInd, bInd + 1, numTrans + 1); //we only take care of b, not a
+        } else {
+            //otherwise, perform the three possible transformations -- insert b.charAt(bInd), replace a.charAt(aInd) with b.charAt(bInd), or remove a.charAt(aInd)
+            int numTransRemove = makeChoice(a, b, memo, aInd + 1, bInd, numTrans + 1); //we only take care of a, not b
+            int numTransReplace = makeChoice(a, b, memo, aInd + 1, bInd + 1, numTrans + 1); //we take care of a and b
+            int numTransInsert = makeChoice(a, b, memo, aInd, bInd + 1, numTrans + 1); //we only take care of b, not a
+            //memoize lowest numTrans and return min
+            memo[bInd][aInd] = Math.min(Math.min(numTransRemove, numTransReplace), numTransInsert);
         }
-        //otherwise, perform the three possible transformations -- insert b.charAt(bInd), replace a.charAt(aInd) with b.charAt(bInd), or remove a.charAt(aInd)
-        int numTransRemove = makeChoice(a, b, memo, aInd + 1, bInd, numTrans + 1); //we only take care of a, not b
-        int numTransReplace = makeChoice(a, b, memo, aInd + 1, bInd + 1, numTrans + 1); //we take care of a and b
-        int numTransInsert = makeChoice(a, b, memo, aInd, bInd + 1, numTrans + 1); //we only take care of b, not a
-
-        //memoize lowest numTrans and return min
-        memo[numTrans][aInd] = Math.min(Math.min(numTransRemove, numTransReplace), numTransInsert);
-        return memo[numTrans][aInd];
+        return memo[bInd][aInd];
     }
 
 }
