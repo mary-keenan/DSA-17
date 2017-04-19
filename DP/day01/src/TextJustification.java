c4eaf7edefac1249c46bed247eb0c82ccade2f52
mary-keenan <mary.keenan@students.olin.edu>
2017-04-19 01:37:15 -0400

    going to try global variable for memo in textJust

--- DP/day01/src/TextJustification.java
+++ DP/day01/src/TextJustification.java
@@ -1,52 +1,65 @@
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class TextJustification {
 
+    public static ArrayList<ArrayList> splitCostList;
+
     public static List<Integer> justifyText(String[] w, int m) {
-        ArrayList bob = new ArrayList();
-        for (int i = 0; i < w.length; i++) bob.add(0, null);
-        List<Integer> lineBreaks = makeChoice(w, m, 0, 0, bob);
+        splitCostList = new ArrayList(w.length);
+        for (int i = 0; i < w.length; i++) splitCostList.add(0, null);
+        List<Integer> lineBreaks = makeChoice(w, m, 0, 0, splitCostList);
+//        Collections.sort(lineBreaks);
         return lineBreaks;
     }
-
 
     private static List<Integer> makeChoice(String[] w, int m, int currWordInd, int currCost, ArrayList<ArrayList> splitCostList) {
         List<Integer> splitIndices = new ArrayList<>();
         //check memo
-        if (splitCostList.size() > 0 && splitCostList.get(currWordInd) != null) return splitCostList.get(currWordInd); //TODO: should I predefine vals? How do I add to specific indices if it's not that long...?
+        if (splitCostList.size() > 0 && splitCostList.get(currWordInd) != null)
+            return splitCostList.get(currWordInd);
 
-        //check base case TODO: does this update splitCostList correctly outside of this recursive call
-        if (currWordInd == w.length) {
+        //check base case -- breaking after last line
+        if (currWordInd > w.length - 2 && currWordInd > 1) {
             ArrayList costIndicesList = new ArrayList<>();
             costIndicesList.add(0, currCost);
-            costIndicesList.add(1, new ArrayList<>());
-            splitCostList.add(currWordInd, costIndicesList);
-            return new ArrayList<>();
+            ArrayList indicesList = new ArrayList();
+            indicesList.add(currWordInd);
+            costIndicesList.add(1, indicesList);
+            splitCostList.add(currWordInd, costIndicesList); //no more
+            return splitIndices; //empty array list
         }
 
-        //given a starting index, what is the best way to split the rest of the lines?
-        //initialize variablese we're tracking
-        int minCost = Integer.MAX_VALUE;
+        //TODO: Given a starting index, what is the best way to split the rest of the lines?
+
+        //initialize variables we're tracking
         int currLineSum = w[currWordInd].length();
+        int minCost = calculateCost(w, m, 0, 0); //minCost is only this word in the line
         int nextPointerInd = currWordInd + 1;
+
         //loop until options are no longer valid
-        while (currLineSum < m && nextPointerInd < w.length) {
+        while (nextPointerInd < w.length) {
             int nextWordLen = w[nextPointerInd].length();
-            if (currLineSum + nextWordLen <= m) { //make sure we won't go past the line limit
+            if (currLineSum + nextWordLen + 1 <= m) { //make sure we won't go past the line limit -- set nextPointerInd to w.length if it does to end it
                 currLineSum += nextWordLen + 1; //account for space between words
                 int currLineCost = currCost + calculateCost(w, m, currWordInd, nextPointerInd);
                 //make recursive call -- we want the final cost, and if it's lower, we want to keep the returned list
+                //TODO: not using wordLineBreaks makes me uncomfortable...but it seems unnecessary for now
+                System.out.println(nextPointerInd);
                 List<Integer> wordLineBreaks = new ArrayList<>(makeChoice(w, m, nextPointerInd + 1, currLineCost, splitCostList));
-                //check its cost in splitCostList -- is it worth pursuing?
-                if ((int) splitCostList.get(nextPointerInd).get(0) < minCost) { //this path is best so far -- it's worth remembering
-                    wordLineBreaks.add(currWordInd + 1); //breaks after this word
+                //check path's cost in splitCostList -- is it worth keeping?
+                int futureCost = (int) splitCostList.get(nextPointerInd + 1).get(0);
+                if (futureCost <= minCost) { //this path is best so far -- might want to put in memo if it stays the best
+                    wordLineBreaks.add(currWordInd);
                     splitIndices = wordLineBreaks; //update current fave list
-                    minCost = (int) splitCostList.get(nextPointerInd).get(0); //update current minCost
+                    minCost = futureCost; //update current minCost
                 }
-            }
+            } else nextPointerInd = w.length; //we've looked at all of the viable options already, let's break out of this joint
+//            System.out.println(currWordInd);
+//            System.out.println(minCost);
             nextPointerInd++;
-
         }
         //save the best thing we found so far and get out of here
         ArrayList bestPair = new ArrayList();
@@ -61,7 +74,7 @@ public class TextJustification {
         for (int wordNum = startWordInd; wordNum <= endWordInd; wordNum++) {
             charSum += w[wordNum].length() + 1; //need to put in a space
         }
-        int cost = (int) Math.pow(m - charSum - 1, 3); //take off the space at the end by subtracting 1
+        int cost = (int) Math.pow(m - charSum + 1, 3); //take off the space at the end by subtracting 1 from charSum (which is just adding to m)
         return cost;
     }
 }
