7724d9cc92b0d7e947313e9e340773c180e8a8ac
mary-keenan <mary.keenan@students.olin.edu>
2017-04-19 13:07:41 -0400

    very close to done with text just -- actual answers only vary a little from expected answers -- a few indices are +1 what they should be, but not many

--- DP/day01/src/TextJustification.java
+++ DP/day01/src/TextJustification.java
@@ -1,76 +1,73 @@
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 
 public class TextJustification {
 
-    public static ArrayList<ArrayList> splitCostList;
+    public static ArrayList<ArrayList> splitLinesList;
+    public static ArrayList<Integer> costList;
 
     public static List<Integer> justifyText(String[] w, int m) {
-        splitCostList = new ArrayList(w.length);
-        for (int i = 0; i < w.length; i++) splitCostList.add(0, null);
+        splitLinesList = new ArrayList(w.length);
+        costList = new ArrayList<>(w.length);
+        for (int i = 0; i < w.length; i++) {
+            splitLinesList.add(0, null);
+            costList.add(0, null);
+        }
         List<Integer> lineBreaks = makeChoice(w, m, 0, 0);
-//        Collections.sort(lineBreaks);
         return lineBreaks;
     }
 
     private static List<Integer> makeChoice(String[] w, int m, int currWordInd, int currCost) {
-        List<Integer> splitIndices = new ArrayList<>();
-        //check memo
-        if (splitCostList.size() > 0 && splitCostList.get(currWordInd) != null)
-            return splitCostList.get(currWordInd);
+        ArrayList<Integer> breakIndices = new ArrayList<>();
 
-        //check base case -- breaking after last line
-        if (currWordInd > w.length - 2 && currWordInd > 1) {
-            ArrayList costIndicesList = new ArrayList<>();
-            costIndicesList.add(0, currCost);
-            ArrayList indicesList = new ArrayList();
-            indicesList.add(currWordInd);
-            costIndicesList.add(1, indicesList);
-            splitCostList.add(currWordInd, costIndicesList); //no more
-            return splitIndices; //empty array list
-        }
+        //check memo
+        if (splitLinesList.size() > 0 && splitLinesList.get(currWordInd) != null)
+            return splitLinesList.get(currWordInd);
 
         //TODO: Given a starting index, what is the best way to split the rest of the lines?
 
-
-
         //initialize variables we're tracking
-        int currLineSum = -1; //no space at the beginning
-        int minCost = calculateCost(w, m, 0, 0); //minCost is only this word in the line
-        int nextPointerInd = currWordInd; //allows for one word lines
+        int currLineSum = w[currWordInd].length(); //no space at the beginning
+        int nextPointerInd = currWordInd + 1; //allows for one word lines
+        int currLineCost = currCost + calculateCost(w, m, currWordInd, nextPointerInd);
+        int minCost = Integer.MAX_VALUE;
+
+        //base case -- no words left, will skip by while loop
+        if (currWordInd > w.length - 2) minCost = currCost;
 
         //loop until options are no longer valid
         while (nextPointerInd < w.length) {
+            //what if we split on this line?
+            ArrayList<Integer> wordLineBreaks = new ArrayList<>(makeChoice(w, m, nextPointerInd, currLineCost));
+            //check path's cost in splitCostList -- is it worth keeping?
+            int futureCost = costList.get(nextPointerInd);
+            if (futureCost <= minCost) { //this path is best so far -- might want to put in memo if it stays the best
+                breakIndices = wordLineBreaks; //update current fave list
+                minCost = futureCost; //update current minCost
+            }
+            //now add next word if possible
             int nextWordLen = w[nextPointerInd].length();
             if (currLineSum + nextWordLen + 1 <= m) { //make sure we won't go past the line limit -- set nextPointerInd to w.length if it does to end it
                 currLineSum += nextWordLen + 1; //account for space between words
-                int currLineCost = currCost + calculateCost(w, m, currWordInd, nextPointerInd);
-                //make recursive call -- we want the final cost, and if it's lower, we want to keep the returned list
-                List<Integer> wordLineBreaks = new ArrayList<>(makeChoice(w, m, nextPointerInd + 1, currLineCost));
-                //check path's cost in splitCostList -- is it worth keeping?
-                int futureCost = (int) splitCostList.get(nextPointerInd + 1).get(0);
-                if (futureCost <= minCost) { //this path is best so far -- might want to put in memo if it stays the best
-                    wordLineBreaks.add(currWordInd);
-                    splitIndices = wordLineBreaks; //update current fave list
-                    minCost = futureCost; //update current minCost
-                }
-            } else nextPointerInd = w.length; //we've looked at all of the viable options already, let's break out of this joint
-            nextPointerInd++;
+                currLineCost = currCost + calculateCost(w, m, currWordInd, nextPointerInd);
+                nextPointerInd++;
+            } else { //new word would exceed line sum
+                nextPointerInd = w.length; //we've looked at all of the viable options already, let's break out of this joint
+            }
         }
+
         //save the best thing we found so far and get out of here
-        ArrayList bestPair = new ArrayList();
-        bestPair.add(0, minCost);
-        bestPair.add(1, splitIndices);
-        splitCostList.remove(currWordInd); //correct for everything shifting back one when we add to certain index
-        splitCostList.add(currWordInd, bestPair);
-        return splitIndices;
+        breakIndices.add(0, currWordInd);
+        costList.remove(currWordInd); //have to remove first so inserting doesn't move everything down
+        costList.add(currWordInd, minCost);
+        splitLinesList.remove(currWordInd);
+        splitLinesList.add(currWordInd, breakIndices);
+        return breakIndices;
     }
 
     private static Integer calculateCost(String[] w, int m, int startWordInd, int endWordInd) {
         int charSum = 0;
-        for (int wordNum = startWordInd; wordNum <= endWordInd; wordNum++) {
+        for (int wordNum = startWordInd; wordNum < endWordInd; wordNum++) {
             charSum += w[wordNum].length() + 1; //need to put in a space
         }
         int cost = (int) Math.pow(m - charSum + 1, 3); //take off the space at the end by subtracting 1 from charSum (which is just adding to m)
